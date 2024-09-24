8e48e8cb8c604b1ed7826af05ecc758c5437d71d
alitsayyed <alitsayyed@gmail.com>
2024-09-24 11:59:20 -0500

    installed django cors headers for authentication on frontend

--- backend/chatapp/settings.py
+++ backend/chatapp/settings.py
@@ -122,3 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CHANNEL_LAYERS = {
+  'default': {
+    'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    # 'CONFIG': {
+    #   'hosts':[('127.0.0.1', 4200)],
+    # }
+  }
+}
+  
+
